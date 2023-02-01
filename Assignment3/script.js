1081475a85f766ee7a1535eff3e178b0fcf30894
Yigit-Sert <113125947+Yigit-Sert@users.noreply.github.com>
2023-02-01 16:52:00 +0300

    Add files via upload

---
+++ Assignment3/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  $(".buttonWideBoxes .buttonWrapping button").click(function () {
+    $(this).toggleClass("transparentButton blackText blueButton shadow border");
+    $(this).find("i").toggleClass("hiddenIcon");
+  });
+  $(".main nav div button").click(function () {
+    $(".form").slideToggle(400);
+  });
+//   form operations
+  $(".topDownButtons").click(function () {
+    $(this).toggleClass("transparentButton whiteText blueButton shadow border");
+    $(this).find("i").toggleClass("hiddenIcon");
+  });
+});
