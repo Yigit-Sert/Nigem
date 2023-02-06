0a8c6207690748c999c7110517fb6d528d959529
Yigit-Sert <113125947+Yigit-Sert@users.noreply.github.com>
2023-02-06 17:41:56 +0300

    Add files via upload

--- Assignment3/script.js
+++ Assignment3/script.js
@@ -2,23 +2,32 @@ $(document).ready(function () {
   $(".buttonWideBoxes .buttonWrapping button").click(function () {
     //   Check if the siblings has the same class
     if ($(this).hasClass("blueButton")) {
-              
+      $(this).toggleClass(
+        "transparentButton blackText blueButton shadow border"
+      );
+      $(this).find("i").toggleClass("hiddenIcon active");
+    } else {
       $(this).toggleClass(
         "transparentButton blackText blueButton shadow border"
       );
       $(this).find("i").toggleClass("hiddenIcon active");
 
-    } else {
-      $(this).toggleClass("transparentButton blackText blueButton shadow border");
-      $(this).find("i").toggleClass("hiddenIcon active");
-      
-      $(this).parent().siblings("div").children(".blueButton").toggleClass("transparentButton blackText blueButton shadow border");
-      $(this).siblings(".blueButton").toggleClass("transparentButton blackText blueButton shadow border");
+      $(this)
+        .parent()
+        .siblings("div")
+        .children(".blueButton")
+        .toggleClass("transparentButton blackText blueButton shadow border");
+      // chose b/w siblings
+      $(this)
+        .siblings(".blueButton")
+        .toggleClass("transparentButton blackText blueButton shadow border");
+      $(this)
+        .parent()
+        .siblings("div")
+        .find(".active")
+        .toggleClass("hiddenIcon active");
+      $(this).siblings().find(".active").toggleClass("hiddenIcon active");
     }
-    // 
-    /* Main Functionality Here */
-    // $(this).toggleClass("transparentButton blackText blueButton shadow border");
-    //   $(this).find("i").toggleClass("hiddenIcon active");
   });
 
   //   Sliding
@@ -28,6 +37,9 @@ $(document).ready(function () {
   //   form operations
   $(".topDownButtons").click(function () {
     $(this).toggleClass("transparentButton whiteText blueButton shadow border");
-    $(this).find("i").toggleClass("hiddenIcon");
+    $(this).siblings(".blueButton").toggleClass("transparentButton whiteText blueButton shadow border");
+    $(this).find("i").toggleClass("hiddenIcon active");
+    $(this).siblings().children(".active").toggleClass("hiddenIcon active");
   });
+  //   Selecting Operations For Form
 });
